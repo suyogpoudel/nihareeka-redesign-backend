036e483401c8cfb1ab6edc4080208d5e540ce856
Suyog Poudel <suyogpoudel583@gmail.com>
2026-02-14 15:57:53 +0545

    added /api/health endpoint for cron job failures

--- index.js
+++ index.js
@@ -31,6 +31,8 @@ app.use(express.json());
 
 app.use("/api/contact", contactRouter);
 
+app.get("/api/health", (req, res) => res.status(200).send("OK"));
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
